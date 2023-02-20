a3411a7aad6298874f73b44b157c6de34f6a8633
Alberto git config --local user.email albertobela@gmail.com <albertobela@gmail.com>
2023-02-20 07:32:36 -0300

    commit inicial

---
+++ Desktop/calculadora/src/Calculadora.java
@@ -0,0 +1,17 @@
+
+public class Calculadora {
+
+	public static void main(String [] args) {
+		System.out.println("Calculadora");
+		
+		int idade;
+		idade = 37;
+
+		System.out.println(idade);
+
+		idade = (7 * 5) +  2;
+
+		String palavra = "Calculando a idade!";
+		System.out.println(palavra);
+	}
+}
